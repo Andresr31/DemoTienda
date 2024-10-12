d538928f6a16a4908fdb4dbc8722f43a4e34ad2c
Carlos Andrés Rojas Parra <carlos.andres.1031@gmail.com>
2024-10-11 22:00:22 -0500

    Se agrega la gestión de archivo de texto plano

--- src/autonoma/DemoTienda/main/DemoTienda.java
+++ src/autonoma/DemoTienda/main/DemoTienda.java
@@ -5,9 +5,19 @@
 package autonoma.DemoTienda.main;
 
 import autonoma.DemoTienda.models.Administrador;
+import autonoma.DemoTienda.models.Escritor;
+import autonoma.DemoTienda.models.EscritorArchivoTextoPlano;
+import autonoma.DemoTienda.models.Lector;
+import autonoma.DemoTienda.models.LectorArchivoTextoPlano;
+import autonoma.DemoTienda.models.Producto;
 import autonoma.DemoTienda.models.Tienda;
 import autonoma.DemoTienda.views.IniciarSesion;
 import autonoma.DemoTienda.views.VentanaPrincipal;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -19,14 +29,51 @@ public class DemoTienda {
         
         
         
+//        Tienda tienda = new Tienda("Nuevo Milenio", "Calle 20 #7b-13");
+//        Administrador admin = new Administrador("andresr31", "12345678", "Carlos Andrés Rojas Parra", "12454153", "Calle 20");
+//        tienda.setAdministrador(admin);
+//        
+//        VentanaPrincipal ventana = new VentanaPrincipal(tienda);
+//        IniciarSesion login = new IniciarSesion(ventana, true, tienda, ventana);
+//        login.setVisible(true);
+
+//          Lector lectorProductos = new LectorArchivoTextoPlano();
+//          
+//        try {
+//            ArrayList<String> archivo = lectorProductos.leer("productos.txt");
+//            for (String linea : archivo) {
+//                System.out.println(linea);
+//                
+//                String nombre = linea.split(";")[0];
+//                double precio = Double.parseDouble(linea.split(";")[1]);
+//                Producto p = new Producto(nombre, precio);
+//                System.out.println(p);
+//            }
+//            
+//            Escritor escritorProductos = new EscritorArchivoTextoPlano("productos.txt");
+//            archivo.add("Motorola;2000");
+//            escritorProductos.escribir(archivo);
+//            
+//        } catch (IOException ex) {
+//            System.out.println("No veo el archivo");
+//        }
+//    
+//        
+//        
+//      
         Tienda tienda = new Tienda("Nuevo Milenio", "Calle 20 #7b-13");
+        try {
+            tienda.cargarProductos();
+        } catch (IOException ex) {
+            System.out.println("No veo el archivo");
+            JOptionPane.showConfirmDialog(null, "Error al iniciar el programa, no se puede acceder al archivo productos.txt", "Error", JOptionPane.ERROR);
+        }
         Administrador admin = new Administrador("andresr31", "12345678", "Carlos Andrés Rojas Parra", "12454153", "Calle 20");
         tienda.setAdministrador(admin);
         
         VentanaPrincipal ventana = new VentanaPrincipal(tienda);
         IniciarSesion login = new IniciarSesion(ventana, true, tienda, ventana);
         login.setVisible(true);
-        
     }
           
 }
--- src/autonoma/DemoTienda/models/Tienda.java
+++ src/autonoma/DemoTienda/models/Tienda.java
@@ -1,30 +1,34 @@
-
 package autonoma.DemoTienda.models;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Educacion
  */
 public class Tienda {
-    
+
     ////////////////////////////////////////////////////////////////////////////
     // Atributos
     private String nombre;
     private String direccion;
     private Inventario inventario;
     private Administrador administrador;
-    
-    
+
+    private Lector lector;
+
     ////////////////////////////////////////////////////////////////////////////
     // Constructor
-
     public Tienda(String nombre, String direccion) {
         this.nombre = nombre;
         this.direccion = direccion;
-        this.inventario = new Inventario();       
-        
+        this.inventario = new Inventario();
+        this.lector = new LectorArchivoTextoPlano();
+
+        // this.cargarProductos();
     }
     ////////////////////////////////////////////////////////////////////////////
     // Metodos de acceso
@@ -49,7 +53,6 @@ public class Tienda {
         return inventario;
     }
 
-
     public Administrador getAdministrador() {
         return administrador;
     }
@@ -57,62 +60,77 @@ public class Tienda {
     public void setAdministrador(Administrador administrador) {
         this.administrador = administrador;
     }
-    
-    public ArrayList<Producto> obtenerProductos(){
+
+    public ArrayList<Producto> obtenerProductos() {
         return this.inventario.getProductos();
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     // Metodos de gestión de productos o CRUD de productos
-    
     ////////////////////////////////////////////////////////////////////////////
-    public boolean agregarProducto(Producto producto){
+    public boolean agregarProducto(Producto producto) {
         return this.inventario.agregarProducto(producto);
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
-    public Producto buscarProducto(Producto producto){
-        
+    public Producto buscarProducto(Producto producto) {
+
         return this.inventario.buscarProducto(producto);
     }
-    
-    public Producto buscarProducto(long id){
-        
+
+    public Producto buscarProducto(long id) {
+
         return this.inventario.buscarProducto(id);
     }
-    
-    public Producto buscarProducto(String nombre){
-        
+
+    public Producto buscarProducto(String nombre) {
+
         return this.inventario.buscarProducto(nombre);
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
-    public Producto actualizarProducto(long id, Producto producto)
-    {
+    public Producto actualizarProducto(long id, Producto producto) {
         return this.inventario.actualizarProducto(id, producto);
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
-    public Producto eliminarProducto(long id){
-        
+    public Producto eliminarProducto(long id) {
+
         return this.inventario.eliminarProducto(id);
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
-    public String mostrarInventario(){
-       
+    public String mostrarInventario() {
+
         return this.inventario.mostrarInventario();
     }
+
     ////////////////////////////////////////////////////////////////////////////
-    public boolean iniciarSesion(String user, String password){
+    public boolean iniciarSesion(String user, String password) {
         return this.administrador.getUsername().equals(user) && this.administrador.getPassword().equals(password);
     }
-    
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    
-    
-    
-    
-    
+
+    public void cargarProductos() throws IOException {
+
+        ArrayList<String> archivo = this.lector.leer("productos.txt");
+        ArrayList<Producto> productos = this.convertirProductos(archivo);
+        this.inventario.cargarProductos(productos);
+
+    }
+
+    private ArrayList<Producto> convertirProductos(ArrayList<String> archivo) {
+
+        ArrayList<Producto> productos = new ArrayList<>();
+        for (String linea : archivo) {
+
+            String nombre = linea.split(";")[0];
+            double precio = Double.parseDouble(linea.split(";")[1]);
+
+            Producto p = new Producto(nombre, precio);
+            productos.add(p);
+        }
+
+        return productos;
+    }
+
 }
